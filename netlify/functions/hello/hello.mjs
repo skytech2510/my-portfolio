a214d9616cbe868238cf12018f9fa4585da160bd
skytech2510 <skytech2510@gmail.com>
2025-02-27 08:44:44 +0800

    s

--- netlify/functions/hello/hello.mjs
+++ netlify/functions/hello/hello.mjs
@@ -1,3 +1,10 @@
+// Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
 export default (request, context) => {
-    return Response("sdfsdfsdf")
+    try {
+        return new Response("sdsdfsdf")
+    } catch (error) {
+        return new Response(error.toString(), {
+            status: 500,
+        })
+    }
 }
