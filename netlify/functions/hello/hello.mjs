8c6f73a3cf54d36202f40f405c91e3e45816d693
skytech2510 <skytech2510@gmail.com>
2025-02-27 08:38:51 +0800

    update

--- netlify/functions/hello/hello.mjs
+++ netlify/functions/hello/hello.mjs
@@ -1,12 +1,3 @@
 export default (request, context) => {
-    try {
-        const url = new URL(request.url)
-        const subject = url.searchParams.get('name') || 'World'
-
-        return new Response(`Hello ${subject}`)
-    } catch (error) {
-        return new Response(error.toString(), {
-            status: 500,
-        })
-    }
+    return Response("sdfsdfsdf")
 }
